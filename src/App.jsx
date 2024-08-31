8304ab6839caf9e9270be4af49d82df302234537
xibluespider <xibluespider@gmail.com>
2024-08-31 20:44:49 +0530

    configure next-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@nextui-org/react";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div className="text-center my-5">
+        hello <Button>world!</Button>
+      </div>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { NextUIProvider } from "@nextui-org/react";
+
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </StrictMode>
+);
