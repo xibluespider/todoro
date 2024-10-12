8265dd33b6df8f29868861d3d8a39b70cd21dadd
xibluespider <xibluespider@gmail.com>
2024-10-12 18:43:50 +0530

    partially handle new task form submission : capture, print, clear

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,14 +3,27 @@ import { Button, Input, Checkbox, Card, CardBody } from "@nextui-org/react";
 import { Plus, Pencil, Trash } from "lucide-react";
 
 export default function Tasks() {
+  const handleFormSubmit = (event) => {
+    console.log("handleFormSubmit invoked");
+
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const new_task_value = formData.get("new_task_value");
+
+    console.log(new_task_value);
+
+    event.target.reset();
+  };
+
   return (
     <div className="flex flex-col md:w-[60%] mx-auto space-y-5 mt-5 p-2">
-      <div className="flex space-x-3 items-center">
-        <Input />
-        <Button isIconOnly size="sm" className="p-2">
+      <form className="flex space-x-3 items-center" onSubmit={handleFormSubmit}>
+        <Input name="new_task_value" />
+        <Button isIconOnly size="sm" className="p-2" type="submit">
           <Plus />
         </Button>
-      </div>
+      </form>
 
       <Card>
         <CardBody>
