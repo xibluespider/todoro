f43007cc66fd01a5fb38f1b6fed50a7d9e76df94
xibluespider <xibluespider@gmail.com>
2024-10-12 14:30:56 +0530

    implement partial design for tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,44 @@
+import { Button, Input, Checkbox, Card, CardBody } from "@nextui-org/react";
+
+import { Plus, Pencil, Trash } from "lucide-react";
+
 export default function Tasks() {
   return (
-    <>
-      <div>tasks component here</div>
-    </>
+    <div className="flex flex-col md:w-[60%] mx-auto space-y-5 mt-5 p-2">
+      <div className="flex space-x-3 items-center">
+        <Input />
+        <Button isIconOnly size="sm" className="p-2">
+          <Plus />
+        </Button>
+      </div>
+
+      <Card>
+        <CardBody>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-3 mx-3">
+              <Checkbox />
+              <div className="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                inventore in rerum? Nostrum fugit culpa vero aliquid pariatur
+                omnis? Assumenda nulla enim, eaque exercitationem dignissimos
+                nam voluptatum dolor accusamus quo. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nostrum sit animi ad voluptatum,
+                temporibus inventore alias laborum voluptatem pariatur delectus
+                quod error itaque beatae tempora illum eaque? Atque, voluptas
+                sapiente!
+              </div>
+            </div>
+            <div className="flex space-x-2">
+              <Button isIconOnly size="sm" className="p-2">
+                <Pencil />
+              </Button>
+              <Button isIconOnly size="sm" className="p-2">
+                <Trash />
+              </Button>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
