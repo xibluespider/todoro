15d775f49fa2dcd4688f4139ad67748fe58edd8b
xibluespider <xibluespider@gmail.com>
2024-10-20 22:02:44 +0530

    incporate useTaskManager on tasks component. add Task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,25 @@
+import { Button, Checkbox, Card, CardBody } from "@nextui-org/react";
+import { Pencil, Trash } from "lucide-react";
+
+export default function Task({ task, deleteTask, toggleComplete }) {
+  return (
+    <Card>
+      <CardBody>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-3 mx-3">
+            <Checkbox isSelected={task.isComplete} onClick={toggleComplete} />
+            <div className="">{task.taskValue}</div>
+          </div>
+          <div className="flex space-x-2">
+            <Button isIconOnly size="sm" className="p-2">
+              <Pencil />
+            </Button>
+            <Button onClick={deleteTask} isIconOnly size="sm" className="p-2">
+              <Trash />
+            </Button>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
