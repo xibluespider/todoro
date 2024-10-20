3cdc6a0dead816defdd5792eeccaf2385038895a
xibluespider <xibluespider@gmail.com>
2024-10-20 22:02:44 +0530

    incporate useTaskManager on tasks component. add Task component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,27 +1,21 @@
-import { Button, Input, Checkbox, Card, CardBody } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
-import { Plus, Pencil, Trash } from "lucide-react";
+import { Plus } from "lucide-react";
 
-import useLocalStorage from "../hooks/useLocalStorage";
+import Task from "./Task";
+import { useTaskManager } from "../hooks/useTaskManager";
 
 export default function Tasks() {
-  const [tasks, setTasks] = useLocalStorage("todoro-tasks", []);
+  const { tasks, setTasks, addTask, toggleCompleteAt, deleteTaskAt } =
+    useTaskManager();
 
   const handleFormSubmit = (event) => {
-    console.log("handleFormSubmit invoked");
-
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const new_task_value = formData.get("new_task_value");
 
-    console.log(new_task_value);
-
-    const new_task = {
-      isComplete: false,
-      taskValue: new_task_value,
-    };
-    setTasks((prevTasks) => [...prevTasks, new_task]);
+    addTask({ task_value: new_task_value });
 
     event.target.reset();
   };
@@ -29,39 +23,21 @@ export default function Tasks() {
   return (
     <div className="flex flex-col md:w-[60%] mx-auto space-y-5 mt-5 p-2">
       <form className="flex space-x-3 items-center" onSubmit={handleFormSubmit}>
-        <Input name="new_task_value" />
+        <Input name="new_task_value" type="text" />
         <Button isIconOnly size="sm" className="p-2" type="submit">
           <Plus />
         </Button>
       </form>
 
-      <Card>
-        <CardBody>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-3 mx-3">
-              <Checkbox />
-              <div className="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-                inventore in rerum? Nostrum fugit culpa vero aliquid pariatur
-                omnis? Assumenda nulla enim, eaque exercitationem dignissimos
-                nam voluptatum dolor accusamus quo. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Nostrum sit animi ad voluptatum,
-                temporibus inventore alias laborum voluptatem pariatur delectus
-                quod error itaque beatae tempora illum eaque? Atque, voluptas
-                sapiente!
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <Button isIconOnly size="sm" className="p-2">
-                <Pencil />
-              </Button>
-              <Button isIconOnly size="sm" className="p-2">
-                <Trash />
-              </Button>
-            </div>
-          </div>
-        </CardBody>
-      </Card>
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          task={task}
+          index={index}
+          toggleComplete={() => toggleCompleteAt({ index })}
+          deleteTask={() => deleteTaskAt({ index })}
+        />
+      ))}
     </div>
   );
 }
