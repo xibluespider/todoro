d2c5a6f996f1079959a29784cfb14a14e051bf7f
xibluespider <xibluespider@gmail.com>
2024-10-20 22:17:55 +0530

    imlpement handleNewTaskSubmitEvent into hook and incorporate

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,24 +6,15 @@ import Task from "./Task";
 import { useTaskManager } from "../hooks/useTaskManager";
 
 export default function Tasks() {
-  const { tasks, setTasks, addTask, toggleCompleteAt, deleteTaskAt } =
+  const { tasks, handleNewTaskSubmitEvent, toggleCompleteAt, deleteTaskAt } =
     useTaskManager();
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const new_task_value = formData.get("new_task_value");
-
-    addTask({ task_value: new_task_value });
-
-    event.target.reset();
-    event.target.new_task_value.focus();
-  };
 
   return (
     <div className="flex flex-col md:w-[60%] mx-auto space-y-5 mt-5 p-2">
-      <form className="flex space-x-3 items-center" onSubmit={handleFormSubmit}>
+      <form
+        className="flex space-x-3 items-center"
+        onSubmit={handleNewTaskSubmitEvent}
+      >
         <Input name="new_task_value" type="text" />
         <Button isIconOnly size="sm" className="p-2" type="submit">
           <Plus />
