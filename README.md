a8bae0e246366e20dcebf8903ed5f85fbbe93729
xibluespider <xibluespider@gmail.com>
2024-11-02 19:51:59 +0530

    update readme.md and about button

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-# React + Vite
+# Todoro
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**Todoro** is a simple and effective productivity tool that combines a To-Do List, a Pomodoro Timer, and Binaural Beats to help you stay focused and productive. This project is built using React for the frontend and leverages local storage to manage tasks.
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- **To-Do List**: Manage your tasks effortlessly.
+- **Pomodoro Timer**: Stay focused with timed work sessions.
+- **Binaural Beats**: Enhance your concentration withs sounds of frequency.
+
+## Acknowledgments
+
+This project would not have been possible without the help and inspiration from various resources. I would like to express my gratitude to all who have supported me in my journey, especially to the people who teach and explain on YouTube, StackOverflow, GitHub discussions, and Reddit, as well as those responsible for documentation works and libraries.
+
+## Reference
+
+- [how to deploy using gh-pages](https://medium.com/@aishwaryaparab1/deploying-vite-deploying-vite-app-to-github-pages-166fff40ffd3)
+- [to apply dark mode with nextui](https://github.com/nextui-org/nextui/issues/1769#issuecomment-2036092086)
+- [samuelsit/react-frequency](https://github.com/samuelsit/react-frequency)
+- [codevolution : learn react](https://www.youtube.com/@Codevolution)
+- [tailwind docs](https://tailwindcss.com/docs/installation)
+- [nextui docs](https://nextui.org/docs/guide/introduction)
+- [lucide icons](https://lucide.dev/icons/)
--- src/components/AboutButton.jsx
+++ src/components/AboutButton.jsx
@@ -8,15 +8,24 @@ import {
 
 import { useDisclosure } from "@nextui-org/react";
 
+const LinkListItem = ({ href, text }) => (
+  <li className="mb-2">
+    <a href={href} target="_blank" className="text-blue-500 hover:underline">
+      {text}
+    </a>
+  </li>
+);
+
 export const AboutButton = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Button onClick={onOpen} color="default" variant="flat">
         About
       </Button>
       <Modal
-        size="xl"
+        size="5xl"
         isOpen={isOpen}
         onClose={onClose}
         scrollBehavior="inside"
@@ -27,28 +36,78 @@ export const AboutButton = () => {
               <ModalHeader className="flex flex-col gap-1">
                 About Todoro
               </ModalHeader>
-              <ModalBody>
-                <p>
-                  I created this personal project called Todoro which combines a
-                  to-do list, binaural beats, and the Pomodoro technique into
-                  one comprehensive tool. This project aims to enhance
-                  productivity and focus by integrating these proven methods
-                  into a single application.
-                </p>
-                <p>
-                  I am grateful for the resources and tools that helped me build
-                  this project, including React, NextUI, and various other
-                  libraries that made the development process smoother and more
-                  enjoyable.
-                </p>
-                <div className="flex justify-center mt-4">
-                  <a
-                    href="https://github.com/your-github-username/todoro"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {/* <FaGithub size={32} /> */}
-                  </a>
+              <ModalBody className="p-6">
+                <div className="text-center mb-6">
+                  <p className="text-lg font-semibold">
+                    <strong>Todoro</strong> is a simple and effective
+                    productivity tool that combines a to-do list, a Pomodoro
+                    timer, and binaural beats to help you stay focused and
+                    productive. This project is built using React for the
+                    frontend and leverages local storage to manage your tasks
+                    and settings.
+                  </p>
+                </div>
+
+                <div className="mb-6">
+                  <h2 className="text-2xl font-bold mb-4">Features</h2>
+                  <ul className="list-disc list-inside">
+                    <li className="mb-2">
+                      To-Do List: Manage your tasks effortlessly.
+                    </li>
+                    <li className="mb-2">
+                      Pomodoro Timer: Stay focused with timed work sessions.
+                    </li>
+                    <li className="mb-2">
+                      Binaural Beats: Enhance your concentration with soothing
+                      sounds.
+                    </li>
+                  </ul>
+                </div>
+
+                <div>
+                  <h2 className="text-2xl font-bold mb-4">Acknowledgments</h2>
+                  <p className="mb-4">
+                    This project would not have been possible without the help
+                    and inspiration from various resources. I would like to
+                    express my gratitude to all who have supported me in my
+                    journey, especially to the people who teach and explain on
+                    YouTube, StackOverflow, GitHub discussions, and Reddit, as
+                    well as those responsible for documentation works and
+                    libraries.
+                  </p>
+                </div>
+                <div>
+                  <h2 className="text-2xl font-bold mb-4">References</h2>
+                  <ul className="list-disc list-inside">
+                    <LinkListItem
+                      href="https://medium.com/@aishwaryaparab1/deploying-vite-deploying-vite-app-to-github-pages-166fff40ffd3"
+                      text="how to deploy using gh-pages"
+                    />
+                    <LinkListItem
+                      href="https://github.com/nextui-org/nextui/issues/1769#issuecomment-2036092086"
+                      text="to apply dark mode with nextui"
+                    />
+                    <LinkListItem
+                      href="https://github.com/samuelsit/react-frequency"
+                      text="samuelsit/react-frequency"
+                    />
+                    <LinkListItem
+                      href="https://www.youtube.com/@Codevolution"
+                      text="codevolution : learn react"
+                    />
+                    <LinkListItem
+                      href="https://tailwindcss.com/docs/installation"
+                      text="tailwind docs"
+                    />
+                    <LinkListItem
+                      href="https://nextui.org/docs/guide/introduction"
+                      text="nextui docs"
+                    />
+                    <LinkListItem
+                      href="https://lucide.dev/icons/"
+                      text="lucide icons"
+                    />
+                  </ul>
                 </div>
               </ModalBody>
             </>
